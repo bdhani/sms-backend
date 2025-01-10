59ca97b7645e330730d2dc6af77279c1cc8be256
Surat Dhani <bdhanirs26@gmail.com>
2025-01-10 23:31:44 +0530

    next change

--- src/app.js
+++ src/app.js
@@ -7,18 +7,9 @@ const app = express();
 // Allow multiple origins for CORS
 const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000"];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: true
+}))
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
